b96edf812c6adb1d233a4d6ba0df9156ea3a05a8
Shahriar Abdullah Isha <65135485+AbdullahIsha@users.noreply.github.com>
2020-11-27 18:09:37 +0600

    models and db connection

---
+++ Mid_Assignment(OnlineMedicineShop)/models/userModel.js
@@ -0,0 +1,33 @@
+const db = require('./db');
+
+module.exports ={
+
+	validate: function(user, callback){
+		var sql = "select * from users where (username='"+user.uid+"' or email='"+user.uid+"') and password='"+user.password+"'";
+		console.log(sql);
+		db.getResults(sql, function(results){
+			if(results.length > 0){
+				callback(results);
+			}else{
+				callback(false);
+			}
+		});
+	},
+	getById: function(id, callback){
+		var sql = "select * from users WHERE id="+id;
+
+        db.getResults(sql, function(results){
+            callback(results);
+        });
+
+	},
+	updateUser: function(user, callback){
+		var sql= "UPDATE users SET name='"+user.name+"', username ='"+user.username +"',password ='"+user.password +"',image ='"+user.image +"', email='"+user.email +"' where id="+user.id;
+		//console.log(sql);
+		db.execute(sql, function(status){
+			callback(status);
+		});
+	}
+
+
+}
