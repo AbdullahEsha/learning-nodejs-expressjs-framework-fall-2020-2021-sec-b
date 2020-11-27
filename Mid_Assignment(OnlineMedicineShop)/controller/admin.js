51c41181a9b3642a71280bf54d9fffcc8c134fc0
Shahriar Abdullah Isha <65135485+AbdullahIsha@users.noreply.github.com>
2020-11-27 18:51:17 +0600

    successfully logged in.

---
+++ Mid_Assignment(OnlineMedicineShop)/controller/admin.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const userModel = require.main.require("./models/userModel");
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+  if(req.session.user != null){
+    res.render('admin/index');
+  }else{
+    res.redirect('/login');
+  }
+})
+
+
+module.exports = router;
